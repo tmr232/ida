7dc77ce0bdcbfa2d4727ecf9e99d5df638664b2e
Tamir Bahar <tmr232@users.noreply.github.com>
2015-04-20 15:23:51 +0300

    Typo.

--- plugins/localxrefs/localxrefs.py
+++ plugins/localxrefs/localxrefs.py
@@ -172,7 +172,7 @@ class localizedxrefs_t(idaapi.plugin_t):
 				info = r.xrefs[ea]
 	
 				if not fmt:
-					fmt = "%%s   %%s   0x%08X %%-%ds   %%s\n" % (len(info['offset']) + 15)
+					fmt = "%%s   %%s   0x%%08X %%-%ds   %%s\n" % (len(info['offset']) + 15)
 
 				idaapi.msg(fmt % (info['direction'], info['type'], info['ea'], info['offset'], info['text']))
 	
